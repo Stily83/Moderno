a3eb1c3e299cc41e0f36a1d874a04d6ff5f1f854
Stily83 <ya.stily@gmail.com>
2020-05-20 17:03:30 +0500

    added product-slider & product

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,21 @@
 $(function(){
+
+    $(".rate-star").rateYo({
+        rating: 5,
+        readOnly: true,
+        starWidth: "12px",
+    });
+
+    
     var mixer = mixitup('.products__inner-box');
 
     
+    $('.product-slider__inner').slick({
+        dots: true,   
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+    });
+
+
 });
